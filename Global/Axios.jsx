e5aa1d189eca9e2ac855e0159f3ddbefd322a328
sameer4712 <sameerptmc@gmail.com>
2025-09-15 15:41:54 +0530

    15-09-2025

--- Global/Axios.jsx
+++ Global/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:3200",
-    withCredentials:true
+    baseURL: "http://3.110.193.205:3200",
+    withCredentials: true
 })
 export default api;
